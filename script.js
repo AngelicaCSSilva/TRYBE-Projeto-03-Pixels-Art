45de81cc7e49a404ad90c57826229a58ef52d6e0
Angélica Silva <angelica.silva9323@gmail.com>
2021-11-22 16:49:28 -0300

    6- Definido black como a cor inicial com classe selected.

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 const pixelBoard = document.querySelector('#pixel-board');
 
-function doSpace() {
-  const space = document.createElement('br');
-  pixelBoard.appendChild(space);
-}
-
 function createPixelBoard() {
   for (let lines = 0; lines < 5; lines += 1) {
     const boardLine = document.createElement('div');
-    boardLine.className = 'line'
+    boardLine.className = 'line';
     for (let collums = 0; collums < 5; collums += 1) {
       const colorBox = document.createElement('div');
       colorBox.className = 'pixel';
@@ -18,4 +13,10 @@ function createPixelBoard() {
   }
 }
 
+function selectDefaultColor() {
+  const blackColor = document.querySelector('#black');
+  blackColor.classList.add('selected');
+}
+
 createPixelBoard();
+selectDefaultColor();
